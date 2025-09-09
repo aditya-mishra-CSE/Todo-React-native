18bc95cebf00ce70fbccf10174f1bf77a4db0330
Aditya Mishra <adityaamishraaa11@gmail.com>
2025-09-09 17:09:10 +0530

    Update RootLayout.tsx

--- Using CLI/src/RootLayout.tsx
+++ Using CLI/src/RootLayout.tsx
@@ -1,1 +1,20 @@
 
+import { ThemeProvider } from './hooks/useTheme';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { NavigationContainer } from '@react-navigation/native';
+import TabsLayout from './tabs/TabsLayout';
+import { CONVEX_URL } from '@env';
+
+const convex = new ConvexReactClient(CONVEX_URL, { unsavedChangesWarning: false });
+
+export default function RootLayout() {
+  return (
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <NavigationContainer>
+          <TabsLayout />
+        </NavigationContainer>
+      </ThemeProvider>
+    </ConvexProvider>
+  );
+}
